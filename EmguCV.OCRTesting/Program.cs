ba2e95515b48ea529f066fd810f4b053bd35b18c
Atanas Marinov <Spirital@gmail.com>
2018-05-31 16:10:37 +0300

    Initial commit to catch-up with local development

---
+++ EmguCV.OCRTesting/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Cuda;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using Emgu.CV.XFeatures2D;
+
+namespace EmguCV.OCRTesting
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //using (Mat image = CvInvoke.Imread("../../../characters/higher-res.jpg"))
+            //{
+            //    var scaledImage = new Mat();
+            //    CvInvoke.Resize(image, scaledImage, Size.Empty, 1.5, 1.5);
+
+            //    DetectLinesInImage(scaledImage);
+            //}
+
+            //using (Image<Bgr, byte> modelImage = new Image<Bgr, byte>("../../../characters/higher-res.jpg"))
+            //{
+            //    DetectRectangles(modelImage);
+            //    DetectLinesInImage(modelImage);
+            //}
+
+            Console.WriteLine("Starting image recognition");
+
+            Task.Factory.StartNew(() =>
+            {
+                using (Image<Bgr, byte> image =
+                    new Image<Bgr, byte>(Path.GetFullPath("../../../characters/characters-and-clues-result.jpg")))
+                {
+                    using (Tesseract tesseractOcrProvider =
+                        new Tesseract("C:\\Program Files (x86)\\Tesseract-OCR\\tessdata", "eng", OcrEngineMode.Default))
+                    {
+                        tesseractOcrProvider.SetImage(image);
+                        tesseractOcrProvider.Recognize();
+                        Tesseract.Character[] characters = tesseractOcrProvider.GetCharacters();
+                        string text = tesseractOcrProvider.GetBoxText();
+                        Console.WriteLine(text);
+                    }
+                }
+            }).Wait();
+
+            Console.WriteLine("Called async image recognition");
+        }
+
+        private static void DetectLinesInImage(Mat modelImage)
+        {
+            Mat destinationImage = new Mat();
+            destinationImage.Create(modelImage.Rows, modelImage.Cols, modelImage.Depth, 1);
+            Mat greyscaleImage = new Mat();
+            CvInvoke.CvtColor(modelImage, greyscaleImage, ColorConversion.Bgr2Gray);
+            //ImageViewer.Show(greyscaleImage);
+
+            Mat detectedEdges = new Mat();
+            CvInvoke.GaussianBlur(greyscaleImage, detectedEdges, new Size(1, 1), 10);
+            //CvInvoke.Blur(greyscaleImage, detectedEdges, new Size(2, 2), new Point(0, 0));
+            int treshold = 50;
+            CvInvoke.Canny(detectedEdges, detectedEdges, treshold, treshold * 3);
+            ImageViewer.Show(detectedEdges);
+        }
+
+        private static void DetectKeyPoints(Image<Bgr, byte> image)
+        {
+            SURF surfDetector = new SURF(400, 4, 2, false);
+            MKeyPoint[] features = surfDetector.Detect(image.Mat);
+
+            using (Image<Bgr, byte> imageWithFeatures = new Image<Bgr, byte>(image.Bitmap))
+            {
+                foreach (MKeyPoint mKeyPoint in features)
+                {
+                    imageWithFeatures.Draw(".", new Point((int)mKeyPoint.Point.X, (int)mKeyPoint.Point.Y),
+                        FontFace.HersheyComplex, 1, new Bgr(Color.Gray));
+                }
+
+                ImageViewer.Show(imageWithFeatures);
+            }
+        }
+
+        private static void DetectKeyPointsCuda(Image<Bgr, byte> image)
+        {
+            Console.WriteLine(CudaInvoke.HasCuda);
+            using (GpuMat<byte> gpuImage = new GpuMat<byte>(image.Mat))
+            {
+                CudaSURF surfDetector = new CudaSURF(400, 4, 2, false);
+                MKeyPoint[] features = surfDetector.DetectKeyPoints(gpuImage, null);
+
+                using (Image<Bgr, byte> imageWithFeatures = new Image<Bgr, byte>(image.Bitmap))
+                {
+                    foreach (MKeyPoint mKeyPoint in features)
+                    {
+                        imageWithFeatures.Draw(".", new Point((int)mKeyPoint.Point.X, (int)mKeyPoint.Point.Y),
+                            FontFace.HersheyComplex, 1, new Bgr(Color.Gray));
+                    }
+
+                    ImageViewer.Show(imageWithFeatures);
+                }
+            }
+        }
+
+        private static void DetectRectangles(Image<Bgr, byte> image)
+        {
+            UMat uImage = new UMat();
+            CvInvoke.CvtColor(image, uImage, ColorConversion.Bgr2Gray);
+
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uImage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uImage);
+
+            double cannyThresholdLinking = 3.0;
+            double cannyThreshold = 100.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(uImage, cannyEdges, cannyThreshold, cannyThresholdLinking);
+            ImageViewer.Show(cannyEdges);
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+                cannyEdges,
+                1, //Distance resolution in pixel-related units
+                Math.PI / 45.0, //Angle resolution measured in radians.
+                20, //threshold
+                30, //min Line width
+                10); //gap between lines
+
+            List<Rectangle> boxList = new List<Rectangle>();
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, true) > 150)
+                        {
+                            if (approxContour.Size == 4)
+                            {
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(edges[(j + 1) % edges.Length]
+                                        .GetExteriorAngleDegree(edges[j]));
+
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+
+                                if (isRectangle)
+                                {
+                                    RotatedRect currentRectangle = CvInvoke.MinAreaRect(approxContour);
+                                    Rectangle minRectangle = currentRectangle.MinAreaRect();
+                                    int ninetyPercentWidth = minRectangle.Width - (int)(minRectangle.Width * 0.1);
+                                    int ninetyPercentHeight = minRectangle.Height - (int)(minRectangle.Height * 0.1);
+                                    minRectangle.Size = new Size(ninetyPercentWidth, ninetyPercentHeight);
+                                    minRectangle.Offset(5, 5);
+                                    boxList.Add(minRectangle);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < boxList.Count; i++)
+            {
+                var rotatedRect = boxList[i];
+
+                image.Draw(rotatedRect, new Bgr(Color.DarkOrange), 2);
+            }
+            //foreach (RotatedRect rotatedRect in boxList)
+            //{
+            //    if (rotatedRect.Angle > -90 && rotatedRect.Angle < -85)
+            //    {
+            //        rotatedRect.Angle = -90;
+            //    }
+
+            //    image.Draw(rotatedRect, new Bgr(Color.DarkOrange), 2);
+            //}
+            
+            ImageViewer.Show(image);
+        }
+    }
+}
---
+++ EmguCV.SquareDetection/SquareDetection.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+
+namespace EmguCV.SquareDetection
+{
+    public static class SquareDetection
+    {
+        // Adapted from https://github.com/alyssaq/opencv
+
+        public const int Treshold = 50;
+        public const int TresholdRatio = 3;
+
+        public static void Main()
+        {
+            using (Mat sourceImage = CvInvoke.Imread("../../../characters/characters-and-clues.jpg"))
+            {
+                Mat scaledImage = new Mat();
+                CvInvoke.Resize(sourceImage, scaledImage, Size.Empty, 1.5, 1.5);
+
+                IEnumerable<Rectangle> detectedRectangles = DetectSquares(scaledImage);
+                Image<Bgr, byte> destinationImage = scaledImage.ToImage<Bgr, byte>();
+
+                foreach (Rectangle rectangle in detectedRectangles)
+                {
+                    destinationImage.Draw(rectangle, new Bgr(Color.DarkOrange), 2);
+                }
+
+                ImageViewer.Show(destinationImage);
+                destinationImage.Save("../../../characters/characters-and-clues-result.jpg");
+            }
+        }
+
+        public static IEnumerable<Rectangle> DetectSquares(Mat sourceImage)
+        {
+            //Mat blurredImage = new Mat();
+            //CvInvoke.MedianBlur(sourceImage, blurredImage, 9);
+
+            //Mat greyscaleImage = new Mat();
+            //CvInvoke.CvtColor(blurredImage, greyscaleImage, ColorConversion.Bgr2Gray);
+
+            //CvInvoke.Canny(greyscaleImage, greyscaleImage, 5, Treshold, 3, true);
+            //CvInvoke.Dilate(greyscaleImage, greyscaleImage, new Mat(), new Point(-1, -1), 3, BorderType.Default, new MCvScalar(255, 255, 255));
+
+            //ImageViewer.Show(greyscaleImage);
+
+            Mat destinationImage = new Mat();
+            destinationImage.Create(sourceImage.Rows, sourceImage.Cols, sourceImage.Depth, 1);
+            Mat greyscaleImage = new Mat();
+            CvInvoke.CvtColor(sourceImage, greyscaleImage, ColorConversion.Bgr2Gray);
+            //ImageViewer.Show(greyscaleImage);
+
+            Mat detectedEdges = new Mat();
+            CvInvoke.GaussianBlur(greyscaleImage, detectedEdges, new Size(1, 1), 1);
+            int treshold = 50;
+            CvInvoke.Canny(detectedEdges, detectedEdges, treshold, treshold * 3);
+            CvInvoke.Dilate(detectedEdges, detectedEdges, new Mat(), new Point(-1, -1), 3, BorderType.Default, new MCvScalar(255, 255, 255));
+
+            ImageViewer.Show(detectedEdges);
+
+            List<Rectangle> boxList = new List<Rectangle>();
+            List<LineSegment2D> lines = new List<LineSegment2D>();
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(detectedEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    using (VectorOfPoint approx = contours[i])
+                    {
+                        CvInvoke.ApproxPolyDP(approx, approxContour, CvInvoke.ArcLength(approx, true) * 0.05, true);
+                        Point[] pts = approxContour.ToArray();
+                        LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+                        lines.AddRange(edges);
+
+                        if (CvInvoke.ContourArea(approxContour, true) > 500)
+                        {
+                            if (approxContour.Size >= 2)
+                            {
+                                bool isRectangle = true;
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(edges[(j + 1) % edges.Length]
+                                        .GetExteriorAngleDegree(edges[j]));
+
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+
+                                if (isRectangle)
+                                {
+                                    RotatedRect currentRectangle = CvInvoke.MinAreaRect(approxContour);
+                                    Rectangle minRectangle = currentRectangle.MinAreaRect();
+                                    int ninetyPercentWidth = minRectangle.Width - (int)(minRectangle.Width * 0.1);
+                                    int ninetyPercentHeight = minRectangle.Height - (int)(minRectangle.Height * 0.1);
+                                    minRectangle.Size = new Size(ninetyPercentWidth, ninetyPercentHeight);
+                                    minRectangle.Offset(5, 5);
+                                    boxList.Add(minRectangle);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return boxList;
+        }
+    }
+}
